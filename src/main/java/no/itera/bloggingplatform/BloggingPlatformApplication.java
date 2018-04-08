d7d19169e4adab03be935d60c318c4f95826b015
stanislav.papco <stanislav.papco@itera.no>
2018-04-08 11:03:48 +0200

    feat: initial commit
     - structure generated by spring-initializr
     - spring-web and spring-test added as dependencies

---
+++ src/main/java/no/itera/bloggingplatform/BloggingPlatformApplication.java
@@ -0,0 +1,12 @@
+package no.itera.bloggingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingPlatformApplication.class, args);
+	}
+}
---
+++ src/test/java/no/itera/bloggingplatform/BloggingPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package no.itera.bloggingplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BloggingPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
