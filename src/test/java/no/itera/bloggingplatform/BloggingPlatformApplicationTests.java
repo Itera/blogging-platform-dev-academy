013d0265e380c67283de0257466007ccea24dabd
Pavol Rajzak <pavol.rajzak@itera.no>
2018-04-16 07:49:16 +0200

    feat: add spring stereotypes for repository

--- src/test/java/no/itera/bloggingplatform/BloggingPlatformApplicationTests.java
+++ src/test/java/no/itera/bloggingplatform/BloggingPlatformApplicationTests.java
@@ -3,14 +3,18 @@ package no.itera.bloggingplatform;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static no.itera.bloggingplatform.constant.ProfileDefinition.IN_MEMORY_STORAGE;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(IN_MEMORY_STORAGE)
 public class BloggingPlatformApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
