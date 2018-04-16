ea1ac469d534fc64e26b57f5e6d76edb289a6dd3
Pavol Rajzak <pavol.rajzak@itera.no>
2018-04-16 07:49:16 +0200

    feat: add spring stereotypes for repository

--- src/main/java/no/itera/bloggingplatform/repository/memory/InMemCommentRepository.java
+++ src/main/java/no/itera/bloggingplatform/repository/memory/InMemCommentRepository.java
@@ -2,10 +2,16 @@ package no.itera.bloggingplatform.repository.memory;
 
 import no.itera.bloggingplatform.model.Comment;
 import no.itera.bloggingplatform.repository.CommentRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static no.itera.bloggingplatform.constant.ProfileDefinition.IN_MEMORY_STORAGE;
+
+@Repository
+@Profile(IN_MEMORY_STORAGE)
 public class InMemCommentRepository extends DefaultInMemoryRepository<Comment> implements CommentRepository {
 
     @Override
