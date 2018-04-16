7ef23a5c0c0da2f7270be4fd46a67d16e03efe32
Pavol Rajzak <pavol.rajzak@itera.no>
2018-04-16 07:50:25 +0200

    feat: add service layer

---
+++ src/main/java/no/itera/bloggingplatform/service/impl/CommentServiceImpl.java
@@ -0,0 +1,45 @@
+package no.itera.bloggingplatform.service.impl;
+
+import no.itera.bloggingplatform.model.Comment;
+import no.itera.bloggingplatform.repository.CommentRepository;
+import no.itera.bloggingplatform.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private CommentRepository commentRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public List<Comment> findCommentsForPost(Long postId) {
+        return commentRepository.findByPostId(postId);
+    }
+
+    @Override
+    public Comment createCommentForPostId(Long postId, Comment comment) {
+        if (comment.getKey() != null) {
+            return comment;
+        }
+        comment.setPostId(postId);
+        return commentRepository.create(comment);
+    }
+
+    @Override
+    public Comment findCommentById(Long commentId) {
+        return commentRepository.read(commentId);
+    }
+
+    @Override
+    public Comment deleteCommentById(Long commentId) {
+        return commentRepository.delete(commentId);
+    }
+
+}
