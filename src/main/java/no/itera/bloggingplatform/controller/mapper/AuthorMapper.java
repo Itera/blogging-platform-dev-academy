eb22404d5dc461617025d60a7de3fe32f3101d0d
stanislav.papco <stanislav.papco@itera.no>
2018-05-13 22:08:15 +0200

    feat: create DTOs with mappers
     - added immutables dependency

---
+++ src/main/java/no/itera/bloggingplatform/controller/mapper/AuthorMapper.java
@@ -0,0 +1,33 @@
+package no.itera.bloggingplatform.controller.mapper;
+
+import no.itera.bloggingplatform.controller.dto.AuthorDto;
+import no.itera.bloggingplatform.controller.dto.ImmutableAuthorDto;
+import no.itera.bloggingplatform.model.Author;
+
+public final class AuthorMapper {
+
+  private AuthorMapper() {
+  }
+
+  public static AuthorDto mapToApi(Author author) {
+    return ImmutableAuthorDto.builder()
+        .id(author.getKey())
+        .userName(author.getUserName())
+        .firstName(author.getFirstName())
+        .lastName(author.getLastName())
+        .email(author.getEmail())
+        .phone(author.getPhone())
+        .build();
+  }
+
+  public static Author mapToDomain(AuthorDto authorDto) {
+    Author author = new Author();
+    author.setKey(authorDto.getId());
+    author.setUserName(authorDto.getUserName());
+    author.setFirstName(authorDto.getFirstName());
+    author.setLastName(authorDto.getLastName());
+    author.setEmail(authorDto.getEmail());
+    author.setPhone(authorDto.getPhone());
+    return author;
+  }
+}
