ca531d4390c1b040355371aca075f06ced7a6f28
stanislav.papco <stanislav.papco@itera.no>
2018-05-15 18:02:45 +0200

    feat: autowire service layer into controllers

--- src/main/java/no/itera/bloggingplatform/controller/AuthorController.java
+++ src/main/java/no/itera/bloggingplatform/controller/AuthorController.java
@@ -1,6 +1,7 @@
 package no.itera.bloggingplatform.controller;
 
-import static java.util.Collections.singletonList;
+import static no.itera.bloggingplatform.controller.mapper.AuthorMapper.mapToApi;
+import static no.itera.bloggingplatform.controller.mapper.AuthorMapper.mapToDomain;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
@@ -9,62 +10,64 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import no.itera.bloggingplatform.controller.dto.AuthorDto;
-import no.itera.bloggingplatform.controller.dto.ImmutableAuthorDto;
+import no.itera.bloggingplatform.controller.mapper.AuthorMapper;
+import no.itera.bloggingplatform.service.AuthorService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
 
 @RestController
 @RequestMapping(value = "/author", produces = APPLICATION_JSON_VALUE)
 public class AuthorController {
 
+  private AuthorService authorService;
+
+  @Autowired
+  public AuthorController(AuthorService authorService) {
+    this.authorService = authorService;
+  }
+
   @RequestMapping(method = GET)
   @ResponseStatus(HttpStatus.OK)
   public List<AuthorDto> fetchAllAuthors() {
-    return singletonList(ImmutableAuthorDto.builder()
-        .id(1L)
-        .userName("findAll")
-        .firstName("John")
-        .lastName("Doe")
-        .email("")
-        .phone("")
-        .build()
-    );
+    return authorService.listAllAuthors().stream()
+        .map(AuthorMapper::mapToApi)
+        .collect(Collectors.toList());
   }
 
   @RequestMapping(method = GET, value = "/{authorId}")
   @ResponseStatus(HttpStatus.OK)
   public AuthorDto fetchAuthorById(@PathVariable Long authorId) {
-    return ImmutableAuthorDto.builder()
-        .id(authorId)
-        .userName("fetchedByName")
-        .firstName("John")
-        .lastName("Doe")
-        .email("")
-        .phone("")
-        .build();
+    return authorService.listAllAuthors().stream()
+        .filter(author -> author.getKey().equals(authorId))
+        .map(AuthorMapper::mapToApi)
+        .findFirst().orElseThrow(() -> new ResourceAccessException("Author not found by userName"));
   }
 
   @RequestMapping(method = POST)
   @ResponseStatus(HttpStatus.CREATED)
   public AuthorDto createAuthor(@RequestBody AuthorDto author) {
-    return ImmutableAuthorDto.copyOf(author).withId(4L).withUserName("Just Created");
+    return mapToApi(authorService.createAuthor(mapToDomain(author)));
   }
 
   @RequestMapping(method = PUT)
   @ResponseStatus(HttpStatus.OK)
   public AuthorDto updateAuthor(@RequestBody AuthorDto author) {
-    return author;
+    return mapToApi(authorService.updateAuthor(author.getId(), mapToDomain(author)));
   }
 
   @RequestMapping(method = DELETE, value = "/{authorId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteAuthorByUsername(@PathVariable Long authorId) {
+    authorService.deleteAuthor(authorId);
   }
 }
--- src/main/java/no/itera/bloggingplatform/controller/CategoryController.java
+++ src/main/java/no/itera/bloggingplatform/controller/CategoryController.java
@@ -1,6 +1,6 @@
 package no.itera.bloggingplatform.controller;
 
-import static java.util.Arrays.asList;
+import static no.itera.bloggingplatform.controller.mapper.CategoryMapper.mapListToApi;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -8,8 +8,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import java.util.List;
 
 import no.itera.bloggingplatform.controller.dto.CategoryDto;
-import no.itera.bloggingplatform.controller.dto.ImmutableCategoryDto;
+import no.itera.bloggingplatform.service.CategoryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/category", produces = APPLICATION_JSON_VALUE)
 public class CategoryController {
 
+  private CategoryService categoryService;
+
+  @Autowired
+  public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
+
   @RequestMapping(method = GET)
   @ResponseStatus(HttpStatus.OK)
   public List<CategoryDto> fetchAllCategories() {
-    return asList(
-        ImmutableCategoryDto.of(1L, "sport"),
-        ImmutableCategoryDto.of(2L, "movies")
-    );
+    return mapListToApi(categoryService.listAllCategories());
   }
 }
