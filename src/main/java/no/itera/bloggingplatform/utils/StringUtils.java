b48e711d681103ed666074ce38b8f526ca088d37
boris.baranec <boris.baranec@itera.no>
2018-05-29 12:36:53 +0200

    feat: add utility method for joining multiple strings by separator

---
+++ src/main/java/no/itera/bloggingplatform/utils/StringUtils.java
@@ -0,0 +1,23 @@
+package no.itera.bloggingplatform.utils;
+
+import java.util.Collection;
+
+public final class StringUtils {
+
+    private StringUtils() {
+    }
+
+    /**
+     * Joins Strings in elements into one String
+     * by using separator between elements.
+     */
+    public static String join(Collection<String> elements, String separator) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String element : elements) {
+            sb.append(element).append(separator);
+        }
+
+        return sb.toString();
+    }
+}
