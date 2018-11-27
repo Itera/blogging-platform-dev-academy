f962e5271c1da4f5ccbe6b293f48834944b8e092
stanislav.papco <stanislav.papco@itera.no>
2018-11-27 14:48:56 +0100

    feat: wire-up endpoints with service layer

--- src/main/java/no/itera/bloggingplatform/controller/CommentController.java
+++ src/main/java/no/itera/bloggingplatform/controller/CommentController.java
@@ -1,18 +1,21 @@
 package no.itera.bloggingplatform.controller;
 
-import static java.util.Collections.singletonList;
+import static no.itera.bloggingplatform.controller.mapper.CommentMapper.mapToApi;
+import static no.itera.bloggingplatform.controller.mapper.CommentMapper.mapToDomain;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import no.itera.bloggingplatform.controller.dto.CommentDto;
-import no.itera.bloggingplatform.controller.dto.ImmutableCommentDto;
+import no.itera.bloggingplatform.controller.mapper.CommentMapper;
+import no.itera.bloggingplatform.service.CommentService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,18 +27,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/comments", produces = APPLICATION_JSON_VALUE)
 public class CommentController {
 
+  private CommentService commentService;
+
+  @Autowired
+  public CommentController(CommentService commentService) {
+    this.commentService = commentService;
+  }
+
   @RequestMapping(value = "post/{postId}", method = GET)
   @ResponseStatus(HttpStatus.OK)
   public List<CommentDto> fetchCommentsForPost(@PathVariable Long postId) {
-    return singletonList(
-        ImmutableCommentDto.of(
-            123L,
-            new Date(),
-            "Joey",
-            "Content of mock comment, fetched for postId:" + postId,
-            12L
-        )
-    );
+    return commentService.findCommentsForPost(postId).stream()
+        .map(CommentMapper::mapToApi)
+        .collect(Collectors.toList());
   }
 
   @RequestMapping(value = "post/{postId}", method = POST)
@@ -44,23 +48,22 @@ public class CommentController {
       @PathVariable Long postId,
       @RequestBody CommentDto comment
   ) {
-    return ImmutableCommentDto.copyOf(comment).withPostId(postId).withId(124L);
+    return mapToApi(
+        commentService.createCommentForPostId(postId, mapToDomain(comment))
+    );
   }
 
   @RequestMapping(value = "/{commentId}", method = GET)
   @ResponseStatus(HttpStatus.OK)
   public CommentDto fetchCommentById(@PathVariable Long commentId) {
-    return ImmutableCommentDto.of(
-        123L,
-        new Date(),
-        "Joey",
-        "Content of mock comment",
-        12L
+    return mapToApi(
+        commentService.findCommentById(commentId)
     );
   }
 
   @RequestMapping(value = "/{commentId}", method = DELETE)
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteCommentById(@PathVariable Long commentId) {
+    commentService.deleteCommentById(commentId);
   }
 }
--- src/main/java/no/itera/bloggingplatform/controller/PostController.java
+++ src/main/java/no/itera/bloggingplatform/controller/PostController.java
@@ -1,7 +1,8 @@
 package no.itera.bloggingplatform.controller;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
+import static no.itera.bloggingplatform.controller.mapper.PostMapper.mapListToApi;
+import static no.itera.bloggingplatform.controller.mapper.PostMapper.mapToApi;
+import static no.itera.bloggingplatform.controller.mapper.PostMapper.mapToDomain;
 
 import static no.itera.bloggingplatform.controller.dto.ImmutablePostDto.copyOf;
 
@@ -11,14 +12,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import no.itera.bloggingplatform.controller.dto.ImmutableAuthorDto;
-import no.itera.bloggingplatform.controller.dto.ImmutableCategoryDto;
-import no.itera.bloggingplatform.controller.dto.ImmutablePostDto;
 import no.itera.bloggingplatform.controller.dto.PostDto;
+import no.itera.bloggingplatform.service.PostService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,66 +32,70 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/posts", produces = APPLICATION_JSON_VALUE)
 public class PostController {
 
+  private PostService postService;
+
+  @Autowired
+  public PostController(PostService postService) {
+    this.postService = postService;
+  }
+
   @RequestMapping(method = GET)
   @ResponseStatus(HttpStatus.OK)
   public List<PostDto> fetchAllPosts() {
-    return singletonList(getMockPost());
+    return mapListToApi(
+        postService.listAllPosts()
+    );
   }
 
   @RequestMapping(method = POST, consumes = APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.CREATED)
   public PostDto createPost(@RequestBody PostDto post) {
-    return copyOf(post).withId(23L).withTitle("Mock title - just created");
+    return mapToApi(
+        postService.createNewPost(mapToDomain(post))
+    );
   }
 
   @RequestMapping(method = PUT)
   @ResponseStatus(HttpStatus.OK)
   public PostDto updatePost(@RequestBody PostDto post) {
-    return post;
+    return mapToApi(
+        postService.updatePost(post.getId(), mapToDomain(post))
+    );
   }
 
   @RequestMapping(value = "/findByAuthor", method = GET)
   @ResponseStatus(HttpStatus.OK)
   public List<PostDto> fetchPostsByAuthor(@RequestParam("author") Long[] authorIds) {
-    return singletonList(copyOf(getMockPost()).withTitle("Mock title - from /findByAuthor"));
+    return mapListToApi(
+        Arrays.stream(authorIds)
+            .map(authorId -> postService.findByAuthor(authorId))
+            .flatMap(List::stream)
+            .collect(Collectors.toList())
+    );
   }
 
   @RequestMapping(value = "/findByCategory", method = GET)
   @ResponseStatus(HttpStatus.OK)
   public List<PostDto> fetchPostsByCategories(@RequestParam("category") Long[] categories) {
-    return singletonList(copyOf(getMockPost()).withTitle("Mock title - from /findByCategory"));
+    return mapListToApi(
+        Arrays.stream(categories)
+            .map(categoryId -> postService.findByCategory(categoryId))
+            .flatMap(List::stream)
+            .collect(Collectors.toList())
+    );
   }
 
   @RequestMapping(value = "/{postId}", method = GET)
   @ResponseStatus(HttpStatus.OK)
   public PostDto fetchPostById(@PathVariable Long postId) {
-    return copyOf(getMockPost()).withId(postId);
+    return mapToApi(
+        postService.findPostById(postId)
+    );
   }
 
   @RequestMapping(value = "/{postId}", method = DELETE)
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deletePostById(@PathVariable Long postId) {
-  }
-
-  private static PostDto getMockPost() {
-    return ImmutablePostDto.builder()
-        .id(22L)
-        .title("Mock title")
-        .perex("mock perex")
-        .content("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut .. ")
-        .author(ImmutableAuthorDto.builder()
-            .id(1L)
-            .userName("updated")
-            .firstName("John")
-            .lastName("Doe")
-            .email("")
-            .phone("")
-            .build())
-        .date(new Date())
-        .categories(asList(
-            ImmutableCategoryDto.of(1L, "sport"),
-            ImmutableCategoryDto.of(2L, "movies")
-        ))
-        .build();
+    postService.deletePost(postId);
   }
 }
