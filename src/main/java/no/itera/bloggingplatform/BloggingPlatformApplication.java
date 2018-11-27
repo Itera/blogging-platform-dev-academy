67a5aec30f50e3b548384960c106598785ff6fa0
stanislav.papco <stanislav.papco@itera.no>
2018-11-27 12:54:38 +0100

    feat: add cors configuration
     - allowing local frontend dev-server on port :9090

--- src/main/java/no/itera/bloggingplatform/BloggingPlatformApplication.java
+++ src/main/java/no/itera/bloggingplatform/BloggingPlatformApplication.java
@@ -2,11 +2,20 @@ package no.itera.bloggingplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BloggingPlatformApplication {
+public class BloggingPlatformApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BloggingPlatformApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BloggingPlatformApplication.class, args);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods("POST", "PUT", "DELETE", "GET", "OPTIONS")
+        .allowedOrigins("http://localhost:9000");
+  }
 }
