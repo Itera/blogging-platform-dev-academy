4c341bc7a9c47f328a6acab1210f404a6e558324
stanislav.papco <stanislav.papco@itera.no>
2018-11-27 18:41:42 +0100

    feat: author endpoint hooked up to service layer

--- src/main/java/no/itera/bloggingplatform/controller/AuthorController.java
+++ src/main/java/no/itera/bloggingplatform/controller/AuthorController.java
@@ -1,6 +1,7 @@
 package no.itera.bloggingplatform.controller;
 
-import static java.util.Collections.singletonList;
+import static no.itera.bloggingplatform.controller.mapper.AuthorMapper.mapToApi;
+import static no.itera.bloggingplatform.controller.mapper.AuthorMapper.mapToDomain;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
@@ -9,10 +10,13 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import no.itera.bloggingplatform.controller.dto.AuthorDto;
-import no.itera.bloggingplatform.controller.dto.ImmutableAuthorDto;
+import no.itera.bloggingplatform.controller.mapper.AuthorMapper;
+import no.itera.bloggingplatform.service.AuthorService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,47 +28,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/authors", produces = APPLICATION_JSON_VALUE)
 public class AuthorController {
 
+  private AuthorService authorService;
+
+  @Autowired
+  public AuthorController(AuthorService authorService) {
+    this.authorService = authorService;
+  }
+
   @RequestMapping(method = GET)
   @ResponseStatus(HttpStatus.OK)
   public List<AuthorDto> fetchAllAuthors() {
-    return singletonList(ImmutableAuthorDto.builder()
-        .id(1L)
-        .userName("findAll")
-        .firstName("John")
-        .lastName("Doe")
-        .email("")
-        .phone("")
-        .build()
-    );
+    return authorService.listAllAuthors().stream()
+        .map(AuthorMapper::mapToApi)
+        .collect(Collectors.toList());
   }
 
   @RequestMapping(method = GET, value = "/{authorId}")
   @ResponseStatus(HttpStatus.OK)
   public AuthorDto fetchAuthorById(@PathVariable Long authorId) {
-    return ImmutableAuthorDto.builder()
-        .id(authorId)
-        .userName("fetchedByName")
-        .firstName("John")
-        .lastName("Doe")
-        .email("")
-        .phone("")
-        .build();
+    return mapToApi(authorService.findAuthorById(authorId));
   }
 
   @RequestMapping(method = POST)
   @ResponseStatus(HttpStatus.CREATED)
   public AuthorDto createAuthor(@RequestBody AuthorDto author) {
-    return ImmutableAuthorDto.copyOf(author).withId(4L).withUserName("Just Created");
+    return mapToApi(authorService.createAuthor(mapToDomain(author)));
   }
 
   @RequestMapping(method = PUT)
   @ResponseStatus(HttpStatus.OK)
   public AuthorDto updateAuthor(@RequestBody AuthorDto author) {
-    return author;
+    return mapToApi(authorService.updateAuthor(author.getId(), mapToDomain(author)));
   }
 
   @RequestMapping(method = DELETE, value = "/{authorId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteAuthorByUsername(@PathVariable Long authorId) {
+    authorService.deleteAuthor(authorId);
   }
 }
