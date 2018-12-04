0153180eab7d8f3787c5789da9758d097345dfe1
stanislav.papco <stanislav.papco@itera.no>
2018-12-04 14:01:23 +0100

    feat: add author validation

--- src/main/java/no/itera/bloggingplatform/controller/dto/AuthorDto.java
+++ src/main/java/no/itera/bloggingplatform/controller/dto/AuthorDto.java
@@ -1,29 +1,36 @@
 package no.itera.bloggingplatform.controller.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import org.immutables.value.Value;
 import org.springframework.lang.Nullable;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-@Value.Immutable
 @JsonSerialize(as = ImmutableAuthorDto.class)
 @JsonDeserialize(as = ImmutableAuthorDto.class)
+@Value.Immutable
 public interface AuthorDto {
 
   @Nullable
   @Value.Parameter
   Long getId();
 
+  @NotBlank
   @Value.Parameter
   String getUserName();
 
+  @Email
   @Value.Parameter
   String getEmail();
 
+  @NotBlank
   @Value.Parameter
   String getFirstName();
 
+  @NotBlank
   @Value.Parameter
   String getLastName();
 
